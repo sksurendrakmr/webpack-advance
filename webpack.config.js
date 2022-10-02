a4798b5396f53e77fc315e0aa4289cc5f385aa56
sksurendrakmr <sksurendrakmr@gmail.com>
2022-10-03 03:27:53 +0530

    asset inline

--- webpack.config.js
+++ webpack.config.js
@@ -11,7 +11,7 @@ module.exports = {
         rules: [
             {
                 test: /\.(png|jpg)$/,
-                type: 'asset/resource'
+                type: 'asset/inline'
             },
             {
                 test: /\.(ttf)$/,
